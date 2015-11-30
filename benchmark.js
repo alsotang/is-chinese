3dda920c50053ddc478117e21c5b73986bb4badf
alsotang <alsotang@gmail.com>
2015-11-30 23:15:41 +0800

    use require('./') instend of require('.')

--- benchmark.js
+++ benchmark.js
@@ -1,5 +1,5 @@
 suite('is_chinese', function () {
-  var isChinese = require('.')
+  var isChinese = require('./')
 
   bench('isChinese("扁担宽，板凳长，扁担想绑在板凳上。")', function() {
     isChinese("扁担宽，板凳长，扁担想绑在板凳上。");
